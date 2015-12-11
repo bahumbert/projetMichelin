56ccde561a4eb396b21a3f658947290c0fcd7e94
bahumbert <humbertb.info@gmail.com>
2015-12-11 17:04:59 +0100

    test

---
+++ src/Ligne.java
@@ -0,0 +1,21 @@
+public class Ligne {
+	String numeroVersion;
+	String idUtilisateur;
+	String date;
+	int lignes;
+	String commentaire;
+	
+	public Ligne(String ligne){
+		
+		// café
+		
+	}
+	
+	public String detectionTicket(){
+		
+		return null;
+		
+	}
+	
+	
+}
---
+++ src/Liste.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Liste {
+
+	/**
+	 * @param args
+	 */
+	
+	ArrayList<Ligne> liste;
+	
+	public Liste(String fichier){
+		System.out.println("new list");
+		// café
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Liste liste = new Liste("./fichier");
+		System.out.println(";-)");
+	}
+
+}
