d477fac6e335ce02c7092c161daa12bc1364d3d7
bahumbert <humbertb.info@gmail.com>
2015-12-12 16:31:58 +0100

    Import de fichier et affichage console

--- src/Ligne.java
+++ src/Ligne.java
@@ -2,19 +2,36 @@ public class Ligne {
 	String numeroVersion;
 	String idUtilisateur;
 	String date;
-	int lignes;
+	String lignes;
 	String commentaire;
 	
 	public Ligne(String ligne){
 		
-		// café
+		String[] split = ligne.split(";");
+		this.numeroVersion = split[0];
+		this.idUtilisateur = split[1];
+		this.date = split[2];
+		this.lignes = split[3];
+		this.commentaire = split[4];
+		
 		
 	}
+	
+	/*public Ligne(String numeroVersion, String commentaire){
+		
+		this.numeroVersion = numeroVersion;
+		this.commentaire = commentaire;
+		
+	}*/
 	
 	public String detectionTicket(){
 		
 		return null;
 		
+	}
+	
+	public String toString(){
+		return this.numeroVersion + " " + this.idUtilisateur + " " + this.date+ " " + this.lignes+ " " + this.commentaire;
 	}
 	
 	
--- src/Liste.java
+++ src/Liste.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 public class Liste {
 
@@ -9,14 +11,69 @@ public class Liste {
 	ArrayList<Ligne> liste;
 	
 	public Liste(String fichier){
-		System.out.println("new list");
-		// café
+
+
+
+		this.liste = new ArrayList<Ligne>();
+		
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			System.out.println("new list");
+			while ((ligne=br.readLine())!=null){
+				this.liste.add(new Ligne(ligne));
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		
+		
 	}
 	
+	/*public Liste(String numeroVersion, String commentaire){
+		System.out.println("new list2");
+
+		liste = new ArrayList<Ligne>();
+		
+		liste.add(new Ligne(numeroVersion, commentaire));
+	}*/
+	
+	public String toString(){
+		
+		String affichage = "";
+		
+		for (Ligne s : liste){
+			affichage += s.toString() + "\n";
+		}
+		return affichage;
+	}
+	
+	public String taille(){
+		
+		return "Il y a " + this.liste.size() + " lignes dans cette liste.";
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Liste liste = new Liste("./fichier");
+		Scanner entree = new Scanner(System.in);
+		System.out.println("Veuillez entrer le chemin du fichier :");
+		String fichier = entree.nextLine();
+		Liste liste = new Liste(fichier);
+		
+		//Liste liste = new Liste("r84004","GDK : WEOSI-195 With your feet on the air and your head on the ground");
+		
+		System.out.println(liste.toString());
+		System.out.println(liste.taille());
+		
 		System.out.println(";-)");
+		
+		entree.close();
 	}
 
 }
